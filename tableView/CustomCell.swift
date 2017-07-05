a236a22a9c567b8bd7714395b20514701301c0c0
范云飞 <786452470@qq.com>
2017-07-05 15:18:54 +0800

    第一次提交

---
+++ tableView/CustomCell.swift
@@ -0,0 +1,78 @@
+//
+//  CustomCell.swift
+//  tableView
+//
+//  Created by 范云飞 on 2017/7/5.
+//  Copyright © 2017年 范云飞. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    //闭包类型 1.()->(),参数，无返回值
+       var btnClickBlock:(()->())?
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = UITableViewCellSelectionStyle.none
+        
+        //添加控件
+        setUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        self.contentView.addSubview(self.ImageView)
+        self.contentView.addSubview(self.label)
+        self.contentView.addSubview(self.button)
+    }
+    
+    
+    lazy var ImageView: UIImageView = {
+        let tempImage = UIImageView(frame:CGRect(x: 10, y: 10, width: 80, height: 80))
+//        tempImage.layer.masksToBounds = true;
+//        tempImage.layer.cornerRadius = 15
+        tempImage.backgroundColor = UIColor.red
+        tempImage.image = UIImage(named:"IMG_0138.jpg")
+        return tempImage
+        
+    }()
+    
+    lazy var label: UILabel = {
+        let tempLab = UILabel(frame: CGRect(x: 100, y: 10, width: 100, height: 30))
+        tempLab.text = "你大爷的"
+        return tempLab
+    }()
+    
+    lazy var button:UIButton = {
+        let tempButton = UIButton(frame:CGRect(x: 210, y: 10, width: 100, height: 30))
+        tempButton.setTitle("button", for: UIControlState.normal)
+        tempButton.setTitleColor(UIColor.red, for: UIControlState.normal)
+        tempButton.backgroundColor = UIColor.black
+        tempButton.addTarget(self, action: #selector(click(sender:)), for: UIControlEvents.touchUpInside)
+        return tempButton
+    }()
+    
+    func click(sender:UIButton) -> Void {
+        if self.btnClickBlock != nil {
+            self.btnClickBlock!()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tableView/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by 范云飞 on 2017/7/5.
+//  Copyright © 2017年 范云飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UINavigationController,UITableViewDelegate,UITableViewDataSource {
+    var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView = UITableView(frame: CGRect(x:0,y:0,width:self.view.frame.size.width,height:self.view.frame.size.height), style: UITableViewStyle.plain)
+        tableView.dataSource = self;
+        tableView.delegate = self;
+        //注册cell
+        tableView.register(CustomCell.classForCoder(), forCellReuseIdentifier: "cellID")
+        self.view.addSubview(tableView)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identify = "cellID"
+        let cell:CustomCell = tableView.dequeueReusableCell(withIdentifier: identify, for: indexPath) as! CustomCell
+        
+//        if (cell == nil) {
+//            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: identify)
+//        }
+        
+//        if cell == nil {
+//            cell = CustomCell(style: UITableViewCellStyle.default, reuseIdentifier: identify)
+//        }
+
+//        cell?.textLabel?.text = "nimeia"
+        weak var weakSelf = self
+        cell.btnClickBlock = {
+            let alertController = UIAlertController(title: "点我干吊", message: "massage", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+            let okAction = UIAlertAction(title: "ok", style: .default) { (UIAlertAction) ->Void in
+                print("确定")
+            }
+            alertController.addAction(cancelAction)
+            alertController.addAction(okAction)
+            weakSelf?.present(alertController, animated: true, completion: nil)
+            
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
